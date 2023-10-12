fab93fee73d3cf3e7c4fdf588c49ac2a825a78ee
dlacuest@stevens.edu <dlacuest@stevens.edu>
2023-10-12 16:19:25 +0000

    Edited Server_Code

--- server_code/Server_Code.py
+++ server_code/Server_Code.py
@@ -15,7 +15,8 @@ def cost_plot(testrun,runcost):
   # Plot it in the normal Matplotlib way
   plt.figure(figsize=(10,6))
   plt.scatter(x,y, color= "crimson", marker='o')
-  
+  plt.xlabel('Test Run', fontsize=14)  # You can adjust the font size (e.g., 14)
+  plt.ylabel('Cost Catalyst', fontsize=14)
   # Return this plot as a PNG image in a Media object, MPL means matplotlib
   return anvil.mpl_util.plot_image()
 @anvil.server.callable
